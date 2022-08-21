683741198c0db0b236f29feadcf2954024f6fa6f
jeronimo-coder <hanapuca.jr@gmail.com>
2022-08-21 19:26:24 -0300

    Actulizacion

--- TP 1/Ejercicio 2/Control/horasCursada.php
+++ TP 1/Ejercicio 2/Control/horasCursada.php
@@ -3,9 +3,12 @@
 $horas = [];
 
 if(isset($_GET)){
+    $lunes = $_GET['horasLunes_form'];
+    $martes = $_GET['horasMartes_form'];
+    $miercoles = $_GET['horasMiercoles_form'];
     $jueves = $_GET['horasJueves_form'];
     $viernes = $_GET['horasViernes_form'];
-    array_push($horas, $jueves, $viernes);
+    array_push($horas, $lunes, $martes, $miercoles, $jueves, $viernes);
 }else{
     echo 'No se ingresaron datos';
 }
--- TP 1/Ejercicio 2/Vista/index.php
+++ TP 1/Ejercicio 2/Vista/index.php
@@ -12,7 +12,10 @@
         <!-- <p>Cuantas dias cursas esta materia?: </p>
         <input type="number" id="diasCursada"><br>
         <input type="button" value="Ok" id="btnDias"><br> -->
-        <h3>Horas de cursada</h3>
+        <h3>Horas de cursada por dia de Programacion Dinamica</h3>
+        Lunes <input type="number" id="horasLunes_form" name="horasLunes_form"><br>
+        Martes <input type="number" id="horasMartes_form" name="horasMartes_form"><br>
+        Miercoles <input type="number" id="horasMiercoles_form" name="horasMiercoles_form"><br>
         Jueves <input type="number" id="horasJueves_form" name="horasJueves_form"><br>
         Viernes <input type="number" id="horasViernes_form" name="horasViernes_form"><br>
         <input type="submit" name="submit" value="Enviar" >
